aaefc260509c33e1adba654da4a557ca66cadd5e
4022s <4022s@users.noreply.local>
2026-01-29 11:55:47 -0800

    added routes

---
+++ app/demo.tsx
@@ -0,0 +1,20 @@
+import ParallaxScrollView from "@/components/parallax-scroll-view";
+import { ThemedText } from "@/components/themed-text";
+import { ThemedView } from "@/components/themed-view";
+import React from "react";
+import { Image } from "react-native";
+
+export default function Demo() {
+  return (
+    <ParallaxScrollView
+      headerBackgroundColor={{ light: "#A1CEDC", dark: "#1D3D47" }}
+      headerImage={
+        <Image source={require("@/assets/images/partial-react-logo.png")} />
+      }
+    >
+      <ThemedView>
+        <ThemedText>Demo Component</ThemedText>
+      </ThemedView>
+    </ParallaxScrollView>
+  );
+}
