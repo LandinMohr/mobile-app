a2256c0a0a1db63a09247336fa4bbde49967f93b
4022s <4022s@users.noreply.local>
2026-02-05 10:10:02 -0800

    i forget

--- app/demo.tsx
+++ app/demo.tsx
@@ -1,10 +1,29 @@
 import ParallaxScrollView from "@/components/parallax-scroll-view";
 import { ThemedText } from "@/components/themed-text";
 import { ThemedView } from "@/components/themed-view";
-import React from "react";
-import { Image } from "react-native";
+import React, { useEffect, useState } from "react";
+import { AppState, Image } from "react-native";
 
 export default function Demo() {
+  const [appStateVisible, setAppStateVisible] = useState(false);
+  useEffect(() => {
+    const subscription = AppState.addEventListener("change", (nextAppState) => {
+      if (
+        nextAppState.match(/inactive|background/) &&
+        nextAppState === "active"
+      ) {
+        console.log("App has come to the foreground!");
+      }
+
+      nextAppState = nextAppState;
+      setAppStateVisible(nextAppState === "active");
+      console.log("AppState", nextAppState);
+    });
+
+    return () => {
+      subscription.remove();
+    };
+  }, []);
   return (
     <ParallaxScrollView
       headerBackgroundColor={{ light: "#A1CEDC", dark: "#1D3D47" }}
